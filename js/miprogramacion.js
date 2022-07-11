5914e19500aeab31380c2f76fa6b0d36825e3335
amarioni <83253601+amarioni@users.noreply.github.com>
2022-07-11 12:33:06 -0300

    Readt

---
+++ js/miprogramacion.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $("#presu").click(function(){
+        $("#center").load("presupuesto.html");
+    });
+
+    $("#amob").click(function(){
+        $("#center").load("amoblamiento.html");
+    });
+
+    $("#driw").click(function(){
+        $("#center").load("driwall.html");
+    });
+});
